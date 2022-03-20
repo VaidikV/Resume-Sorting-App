a061e29f8f11f4247b619090c1e3a14747123885
Vaidik V <63895478+VaidikV@users.noreply.github.com>
2022-03-21 02:26:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,203 @@
+from resume_reader import ResumeReader
+from tkPDFViewer import tkPDFViewer as Pdf
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import *
+
+FONT = ("Ariel", 14, "normal")
+BTN_FONT = ("Ariel", 12, "normal")
+BACKGROUND_COLOR = "#EEF2FF"
+PRIMARY_BTN = "#1C658C"
+SECONDARY_BTN = "#398AB9"
+FONT_FG = "#2C3333"
+
+keyword_file = ""
+domain = ""
+selected_pdfs = []
+domain_based_resumes = []
+resume_objects = []
+
+
+# ----------------------------------------- TOOLS (BACK-END) -----------------------------------------
+# --------- SET SORTING KEYWORDS ---------
+def tech():
+    global keyword_file, domain
+    keyword_file = "tech_skills.txt"
+    domain = "Technology"
+    select_files()
+
+
+def hr():
+    global keyword_file, domain
+    keyword_file = "hr_skills.txt"
+    domain = "Human Resources"
+    select_files()
+
+
+def acc():
+    global keyword_file, domain
+    keyword_file = "accounting_skills.txt"
+    domain = "Accounting"
+    select_files()
+
+
+def mkt():
+    global keyword_file, domain
+    keyword_file = "marketing_skills.txt"
+    domain = "Marketing"
+    select_files()
+
+
+# --------- SORT & CREATE RESUME OBJECTS ---------
+def sort_resumes():
+    global resume_objects
+    for resume in selected_pdfs:
+        resume_object = ResumeReader(resume, keyword_file)
+        resume_objects.append(resume_object)
+
+
+# --------- DOMAIN BASED RESUMES ---------
+def sort_domain_based():
+    global domain_based_resumes
+    domain_based_resumes = []
+    for res in resume_objects:
+        if res.no_of_skills != 0:
+            domain_based_resumes.append(res)
+        else:
+            continue
+    # print(len(domain_based_resumes))
+    # for i in domain_based_resumes:
+    #     # print(i.name)
+
+
+# --------- CHOOSE FILES ---------
+def select_files():
+    global selected_pdfs
+    loading_window.pack()
+    main_window.pack_forget()
+    window.filename = filedialog.askopenfilenames(initialdir=r"C:\Users\Vaidik\Documents", title="Select resumes",
+                                                  filetypes=(("pdf files", "*.pdf"), ("all files", "*.*")))
+    selected_pdfs = list(window.filename)
+    if not selected_pdfs:
+        loading_window.pack_forget()
+        main_window.pack()
+        messagebox.showerror(title="No resume selected", message="Please select at least one resume to proceed")
+    else:
+        sorting_resume_load_screen()
+
+
+# --------- GO BACK TO SORTED RESUME WINDOW ---------
+def back_to_main():
+    global selected_pdfs, domain_based_resumes, resume_objects
+    selected_pdfs = []
+    domain_based_resumes = []
+    resume_objects = []
+    results.pack_forget()
+    main_window.pack()
+
+    for widget in results.winfo_children():
+        widget.destroy()
+
+
+# --------- OPEN PDF ---------
+def view_pdf(path):
+    pdf_window = Toplevel(window)
+    pdf_window.geometry("650x600")
+    pdf_window.title("PDF viewer")
+    make_pdf = Pdf.ShowPdf()
+    make_pdf.img_object_li.clear()
+    place_pdf = make_pdf.pdf_view(pdf_window, pdf_location=path, width=75, height=35)
+    place_pdf.config()
+    place_pdf.grid(row=0, column=0)
+
+
+# --------- JUMP TO FRAME [2] (LOADING WINDOW) ---------
+def sorting_resume_load_screen():
+    sort_resumes()
+    loading_window.pack_forget()
+    sort_domain_based()
+    view_sorted_resumes()
+
+
+# --------- JUMP TO FRAME [3] (VIEW SORTED RESUMES) ---------
+def view_sorted_resumes():
+    results.pack()
+
+    results_head = Label(results, text=f"Resumes sorted according to {domain} domain.",
+                         bg=BACKGROUND_COLOR, font=("times new roman", 22, "bold"), fg=FONT_FG, wraplength=600)
+    results_head.grid(row=0, column=0, padx=20)
+
+    back = Button(results, text="Back", command=back_to_main, bg=SECONDARY_BTN, fg="white", width=15,
+                  font=BTN_FONT)
+    back.grid(row=0, column=1)
+
+    button_list = []
+    for obj in domain_based_resumes:
+        def get_file_loc(res_obj=obj):
+            view_pdf(res_obj.pdf_path)
+
+        button_list.append(Button(results, text=obj.name, bg=SECONDARY_BTN, fg="white", font=BTN_FONT, width=15,
+                                  command=get_file_loc))
+        button_list[domain_based_resumes.index(obj)].grid(row=domain_based_resumes.index(obj) + 1, column=0, pady=15)
+
+
+# ----------------------------------------- UI SETUP (FRONT-END) -----------------------------------------
+# --------- ROOT ---------
+window = Tk()
+window.title("Resume Parser")
+window.config(padx=30, pady=30, bg=BACKGROUND_COLOR)
+# window.geometry("600x500")
+window.geometry("+300+100")
+window.iconbitmap("logo.ico")
+
+# --------- FRAMES ---------
+main_window = Frame(window, bg=BACKGROUND_COLOR)  # F1
+loading_window = Frame(window, bg=BACKGROUND_COLOR)  # F2
+results = Frame(window, bg=BACKGROUND_COLOR)  # F3
+# pdf_window = Frame(window, bg=BACKGROUND_COLOR)  # F4
+
+# --------- FRAME [1] (MAIN WINDOW) ---------
+# [1] LOGO
+canvas = Canvas(main_window, width=150, height=150, bg=BACKGROUND_COLOR)
+canvas.pack()
+img = PhotoImage(file="logo.png")
+canvas.create_image(75, 75, image=img)
+canvas.grid(row=0, column=0, columnspan=2)
+
+# [1] LABELS
+welcome_screen_head = Label(main_window, text="Hire-me!", bg=BACKGROUND_COLOR,
+                            font=("times new roman", 22, "bold"), fg=FONT_FG)
+welcome_screen_head.grid(row=1, column=0, columnspan=2)
+
+welcome_screen_slogan = Label(main_window, text="A powerful resume parser.", bg=BACKGROUND_COLOR,
+                              font=FONT, fg=FONT_FG)
+welcome_screen_slogan.grid(row=2, column=0, columnspan=2)
+
+welcome_screen_start = Label(main_window, text="Choose a domain:",
+                             bg=BACKGROUND_COLOR, font=FONT, pady=40, fg=FONT_FG, wraplength=410)
+welcome_screen_start.grid(row=3, column=0, columnspan=2)
+
+# [1] BUTTONS
+tech_sort = Button(main_window, text="Technology", command=tech, bg=SECONDARY_BTN, fg="white", width=15,
+                   font=BTN_FONT)
+tech_sort.grid(row=4, column=0, padx=10)
+
+hr_sort = Button(main_window, text="Human Resources", command=hr, bg=SECONDARY_BTN, fg="white", width=15,
+                 font=BTN_FONT)
+hr_sort.grid(row=4, column=1, padx=10)
+
+acc_sort = Button(main_window, text="Accounting", command=acc, bg=SECONDARY_BTN, fg="white", width=15,
+                  font=BTN_FONT)
+acc_sort.grid(row=5, column=0)
+
+mkt_sort = Button(main_window, text="Marketing", command=mkt, bg=SECONDARY_BTN, fg="white", width=15,
+                  font=BTN_FONT)
+mkt_sort.grid(row=5, column=1, pady=20)
+
+# --------- FRAME [2] (LOADING WINDOW) ---------
+loading = Label(loading_window, text="Sorting selected resumes. Please wait...",
+                bg=BACKGROUND_COLOR, font=("times new roman", 22, "bold"), fg=FONT_FG, wraplength=490)
+loading.grid(row=0, column=0, pady=180)
+
+main_window.pack()
+window.mainloop()
---
+++ resume_reader.py
@@ -0,0 +1,128 @@
+from pdfminer.high_level import extract_text
+from nltk.corpus import stopwords
+from spacy.matcher import Matcher
+import spacy
+import re
+
+
+# ----------------------------------- EXTRACTING INFO FROM PDF -----------------------------------
+class ResumeReader:
+    def __init__(self, pdf_path, skill_keyword):
+        self.name = ""
+        self.mobile = 0
+        self.email = ""
+        self.skills = ""
+        self.no_of_skills = 0
+        self.education = ""
+        self.pdf_path = pdf_path
+        self.skill_keyword = skill_keyword
+
+        resume_path = self.pdf_path
+        skill_keyword_file = self.skill_keyword
+        raw_text = extract_text(resume_path)
+
+        nlp = spacy.load("en_core_web_sm")  # load pre-trained model
+        doc = nlp(raw_text)
+        noun_chunks = doc.noun_chunks
+        stopwords_from_pdf = set(stopwords.words('english'))  # Grad all general stop words
+        matcher = Matcher(nlp.vocab)  # initialize matcher with a vocab
+        nlp_text = nlp(raw_text)
+
+        # Education Degrees
+        degree_keywords = [
+            'BE', 'B.E.', 'B.E', 'BS', 'B.S',
+            'ME', 'M.E', 'M.E.', 'MS', 'M.S',
+            'BTECH', 'B.TECH', 'M.TECH', 'MTECH',
+            'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII', 'BSc',
+            'B.A.', 'BA', 'B.A', 'MBA', 'M.B.A.', 'M.B.A'
+        ]
+
+        # ---------- EXTRACTING NAME ----------
+        # This function helps in finding a pair of pronouns which are side by side. Used for finding names
+        # First name and Last name are always Proper Nouns
+        pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
+
+        matcher.add("PERSON", [pattern])
+
+        matches = matcher(nlp_text)
+
+        combinations = []
+        for match_id, start, end in matches:
+            span = nlp_text[start:end]
+            combinations.append(span)
+        self.name = combinations[0]
+
+        # ---------- EXTRACTING MOBILE NUMBER ----------
+        phone = re.findall(re.compile(
+            r'(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9]'
+            r'[02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1'
+            r'[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'),
+            raw_text)
+
+        if phone:
+            number = ''.join(phone[0])
+            if len(number) > 10:
+                self.mobile = '+' + number
+            else:
+                self.mobile = number
+
+        # ---------- EXTRACTING EMAIL ----------
+        email = re.findall(r"([^@|\s]+@[^@]+\.[^@|\s]+)", raw_text)
+        if email:
+            try:
+                self.email = email[0].split()[0].strip(';')
+            except IndexError:
+                self.email = "Could not extract email."
+
+        # ---------- EXTRACTING SKILLS ----------
+        # removing stop words and implementing word tokenization
+        tokens = [token.text for token in nlp_text if not token.is_stop]
+        skills = []
+        # reading the txt file
+        with open(skill_keyword_file, "r") as file:
+            skills_raw = file.read().splitlines()
+            for i in skills_raw:
+                skills.append(i.lower())
+        skillset = []
+
+        # check for one-grams (example: python)
+        for token in tokens:
+            # print(token)
+            if token.lower() in skills:
+                skillset.append(token)
+
+        # check for bi-grams and tri-grams (example: machine learning)
+        for token in noun_chunks:
+            # print(token)
+            token = token.text.lower().strip()
+            if token in skills:
+                skillset.append(token)
+        skills_list = [i.title() for i in set([i for i in skillset])]
+        self.skills = ", ".join(skills_list)
+        self.no_of_skills = len(skills_list)
+
+        # ---------- EXTRACTING EDUCATION DEGREE ----------
+        # Sentence Tokenizer
+        nlp_text = [sent.text for sent in nlp_text.sents]
+
+        edu = {}
+        # Extract education degree
+        for index, text in enumerate(nlp_text):
+            for tex in text.split():
+                # Replace all special symbols
+                tex = re.sub(r'[?|$.!,]', r'', tex)
+                if tex.upper() in degree_keywords and tex not in stopwords_from_pdf:
+                    edu[tex] = text
+
+        # Extract year
+        education = []
+        for key in edu.keys():
+            year = re.search(re.compile(r'((20|19)(\d{2}))'), edu[key])
+            if year:
+                education.append((key, ''.join(year[0])))
+            else:
+                education.append(key)
+        try:
+            self.education = f"{education[0][0]}:- {education[0][1]}"
+        except IndexError:
+            self.education = "Couldn't extract education details."
